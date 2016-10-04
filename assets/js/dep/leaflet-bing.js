ae8a1f7d61c0338d6137bc60c451f917c320a48e
Mayank Badola <badola21295@gmail.com>
2016-10-04 11:25:27 +0530

    lint fix to resolve #18

--- assets/js/dep/leaflet-bing.js
+++ assets/js/dep/leaflet-bing.js
@@ -1,150 +1,150 @@
 (function (factory) {
-    if (typeof define === 'function' && define.amd) {
+  if (typeof define === 'function' && define.amd) {
         // AMD
-        define(['leaflet'], factory);
-    } else if (typeof module !== 'undefined') {
+    define(['leaflet'], factory);
+  } else if (typeof module !== 'undefined') {
         // Node/CommonJS
-        module.exports = factory(require('leaflet'));
-    } else {
+    module.exports = factory(require('leaflet'));
+  } else {
         // Browser globals
-        if (typeof this.L === 'undefined')
-            throw 'Leaflet must be loaded first!';
-        factory(this.L);
-    }
-}(function (L) {
+    if (typeof this.L === 'undefined') { throw new Error('Leaflet must be loaded first!'); }
+    factory(this.L);
+  }
+}(L => {
+  L.BingLayer = L.TileLayer.extend({
+    options: {
+      subdomains: [0, 1, 2, 3],
+      type: 'Aerial',
+      attribution: 'Bing',
+      culture: ''
+    },
 
-L.BingLayer = L.TileLayer.extend({
-  options: {
-    subdomains: [0, 1, 2, 3],
-    type: 'Aerial',
-    attribution: 'Bing',
-    culture: ''
-  },
+    initialize: function (key, options) {
+      L.Util.setOptions(this, options);
 
-  initialize: function (key, options) {
-    L.Util.setOptions(this, options);
+      this._key = key;
+      this._url = null;
+      this._providers = [];
+      this.metaRequested = false;
+    },
 
-    this._key = key;
-    this._url = null;
-    this._providers = [];
-    this.metaRequested = false;
-  },
+    tile2quad: function (x, y, z) {
+      let quad = '';
+      for (let i = z; i > 0; i--) {
+        let digit = 0;
+        const mask = 1 << (i - 1);
+        if ((x & mask) !== 0) digit += 1;
+        if ((y & mask) !== 0) digit += 2;
+        quad = quad + digit;
+      }
+      return quad;
+    },
 
-  tile2quad: function (x, y, z) {
-    var quad = '';
-    for (var i = z; i > 0; i--) {
-      var digit = 0;
-      var mask = 1 << (i - 1);
-      if ((x & mask) !== 0) digit += 1;
-      if ((y & mask) !== 0) digit += 2;
-      quad = quad + digit;
-    }
-    return quad;
-  },
-
-  getTileUrl: function (tilePoint) {
-    var zoom = this._getZoomForUrl();
-    var subdomains = this.options.subdomains,
-      s = this.options.subdomains[Math.abs((tilePoint.x + tilePoint.y) % subdomains.length)];
-    return this._url.replace('{subdomain}', s)
+    getTileUrl: function (tilePoint) {
+      const zoom = this._getZoomForUrl();
+      const subdomains = this.options.subdomains;
+      const s = this.options.subdomains[Math.abs((tilePoint.x + tilePoint.y) % subdomains.length)];
+      return this._url.replace('{subdomain}', s)
         .replace('{quadkey}', this.tile2quad(tilePoint.x, tilePoint.y, zoom))
         .replace('{culture}', this.options.culture);
-  },
+    },
 
-  loadMetadata: function () {
-    if (this.metaRequested) return;
-    this.metaRequested = true;
-    var _this = this;
-    var cbid = '_bing_metadata_' + L.Util.stamp(this);
-    window[cbid] = function (meta) {
-      window[cbid] = undefined;
-      var e = document.getElementById(cbid);
-      e.parentNode.removeChild(e);
-      if (meta.errorDetails) {
-        throw new Error(meta.errorDetails);
-        return;
-      }
-      _this.initMetadata(meta);
-    };
-    var urlScheme = (document.location.protocol === 'file:') ? 'http' : document.location.protocol.slice(0, -1);
-    var url = urlScheme + '://dev.virtualearth.net/REST/v1/Imagery/Metadata/'
+    loadMetadata: function () {
+      if (this.metaRequested) return;
+      this.metaRequested = true;
+      const _this = this;
+      const cbid = '_bing_metadata_' + L.Util.stamp(this);
+      window[cbid] = function (meta) {
+        window[cbid] = undefined;
+        const e = document.getElementById(cbid);
+        e.parentNode.removeChild(e);
+        if (meta.errorDetails) {
+          throw new Error(meta.errorDetails);
+        }
+        _this.initMetadata(meta);
+      };
+      const urlScheme = (document.location.protocol === 'file:')
+      ? 'http' : document.location.protocol.slice(0, -1);
+      const url = urlScheme + '://dev.virtualearth.net/REST/v1/Imagery/Metadata/'
           + this.options.type + '?include=ImageryProviders&jsonp=' + cbid +
           '&key=' + this._key + '&UriScheme=' + urlScheme;
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = url;
-    script.id = cbid;
-    document.getElementsByTagName('head')[0].appendChild(script);
-  },
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = url;
+      script.id = cbid;
+      document.getElementsByTagName('head')[0].appendChild(script);
+    },
 
-  initMetadata: function (meta) {
-    var r = meta.resourceSets[0].resources[0];
-    this.options.subdomains = r.imageUrlSubdomains;
-    this._url = r.imageUrl;
-    if (r.imageryProviders) {
-      for (var i = 0; i < r.imageryProviders.length; i++) {
-        var p = r.imageryProviders[i];
-        for (var j = 0; j < p.coverageAreas.length; j++) {
-          var c = p.coverageAreas[j];
-          var coverage = {zoomMin: c.zoomMin, zoomMax: c.zoomMax, active: false};
-          var bounds = new L.LatLngBounds(
-              new L.LatLng(c.bbox[0]+0.01, c.bbox[1]+0.01),
-              new L.LatLng(c.bbox[2]-0.01, c.bbox[3]-0.01)
+    initMetadata: function (meta) {
+      const r = meta.resourceSets[0].resources[0];
+      this.options.subdomains = r.imageUrlSubdomains;
+      this._url = r.imageUrl;
+      if (r.imageryProviders) {
+        for (let i = 0; i < r.imageryProviders.length; i++) {
+          const p = r.imageryProviders[i];
+          for (let j = 0; j < p.coverageAreas.length; j++) {
+            const c = p.coverageAreas[j];
+            const coverage = { zoomMin: c.zoomMin, zoomMax: c.zoomMax, active: false };
+            const bounds = new L.LatLngBounds(
+              new L.LatLng(c.bbox[0] + 0.01, c.bbox[1] + 0.01),
+              new L.LatLng(c.bbox[2] - 0.01, c.bbox[3] - 0.01)
           );
-          coverage.bounds = bounds;
-          coverage.attrib = p.attribution;
-          this._providers.push(coverage);
+            coverage.bounds = bounds;
+            coverage.attrib = p.attribution;
+            this._providers.push(coverage);
+          }
         }
       }
-    }
-    this._update();
-  },
+      this._update();
+    },
 
-  _update: function () {
-    if (this._url === null || !this._map) return;
-    this._update_attribution();
-    L.TileLayer.prototype._update.apply(this, []);
-  },
+    _update: function () {
+      if (this._url === null || !this._map) return;
+      this._update_attribution();
+      L.TileLayer.prototype._update.apply(this, []);
+    },
 
-  _update_attribution: function () {
-    var bounds = L.latLngBounds(this._map.getBounds().getSouthWest().wrap(),this._map.getBounds().getNorthEast().wrap());
-    var zoom = this._map.getZoom();
-    for (var i = 0; i < this._providers.length; i++) {
-      var p = this._providers[i];
-      if ((zoom <= p.zoomMax && zoom >= p.zoomMin) &&
+    _update_attribution: function () {
+      const bounds = L.latLngBounds(this._map.getBounds().getSouthWest().wrap()
+      , this._map.getBounds().getNorthEast().wrap());
+      const zoom = this._map.getZoom();
+      for (let i = 0; i < this._providers.length; i++) {
+        const p = this._providers[i];
+        if ((zoom <= p.zoomMax && zoom >= p.zoomMin) &&
           bounds.intersects(p.bounds)) {
-        if (!p.active && this._map.attributionControl)
-          this._map.attributionControl.addAttribution(p.attrib);
-        p.active = true;
-      } else {
-        if (p.active && this._map.attributionControl)
+          if (!p.active && this._map.attributionControl) {
+            this._map.attributionControl.addAttribution(p.attrib);
+          }
+          p.active = true;
+        } else {
+          if (p.active && this._map.attributionControl) {
+            this._map.attributionControl.removeAttribution(p.attrib);
+          }
+          p.active = false;
+        }
+      }
+    },
+
+    onAdd: function (map) {
+      this.loadMetadata();
+      L.TileLayer.prototype.onAdd.apply(this, [map]);
+    },
+
+    onRemove: function (map) {
+      for (let i = 0; i < this._providers.length; i++) {
+        const p = this._providers[i];
+        if (p.active && this._map.attributionControl) {
           this._map.attributionControl.removeAttribution(p.attrib);
-        p.active = false;
+          p.active = false;
+        }
       }
+      L.TileLayer.prototype.onRemove.apply(this, [map]);
     }
-  },
+  });
 
-  onAdd: function (map) {
-    this.loadMetadata();
-    L.TileLayer.prototype.onAdd.apply(this, [map]);
-  },
-
-  onRemove: function (map) {
-    for (var i = 0; i < this._providers.length; i++) {
-      var p = this._providers[i];
-      if (p.active && this._map.attributionControl) {
-        this._map.attributionControl.removeAttribution(p.attrib);
-        p.active = false;
-      }
-    }
-    L.TileLayer.prototype.onRemove.apply(this, [map]);
-  }
-});
-
-L.bingLayer = function (key, options) {
+  L.bingLayer = function (key, options) {
     return new L.BingLayer(key, options);
-};
+  };
 
-return L.BingLayer;
-
+  return L.BingLayer;
 }));
--- assets/js/lib/util.js
+++ assets/js/lib/util.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 /* global app: true */
 const urllib = require('url');
 const pathlib = require('path');
@@ -12,11 +12,7 @@ function extend(dest, src) {
   let from;
   for (let s = 1; s < arguments.length; s++) {
     from = Object(arguments[s]);
-    for (const key in from) {
-      if (hasOwnProperty.call(from, key)) {
-        to[key] = from[key];
-      }
-    }
+    Object.assign(to, from);
   }
   return to;
 }
@@ -25,7 +21,7 @@ function extend(dest, src) {
  * Make slug from string.
  * Support: en, th, ja
  * @see https://gist.github.com/mathewbyrne/1280286
- * @see http://so-zou.jp/software/tech/programming/tech/regular-expression/meta-character/variable-width-encoding.htm
+ * @see http://so-zou.jp/software/tech/programming/tech/regular-expression/meta-character/letiable-width-encoding.htm
  * @see https://python3.wannaphong.com/2015/12/regular-expression-ภาษาไทย-และภาษาอื่น-python.html
  * @return {String} Slug
  */
@@ -94,11 +90,9 @@ function make_querystring(query) {
  */
 function format_querystring(query) {
   const qs = [];
-  for (const k in query) {
-    if (hasOwnProperty.call(query, k)) {
-      qs.push(k + '=' + query[k]);
-    }
-  }
+  Object.keys(query).forEach(k => {
+    qs.push(k + '=' + query[k]);
+  });
   return qs.join('&');
 }
 
@@ -145,12 +139,12 @@ function build_image_size(url, breakpoints, dppi, screenWidth, screenBreakpoints
 }
 
 function parse_image_url(url) {
-  var data = {};
-  var split = url ? url.split('#') : [];
-  var breakpoints = [];
-  var bp = [];
-  var i;
-  var pt;
+  const data = {};
+  const split = url ? url.split('#') : [];
+  const breakpoints = [];
+  let bp = [];
+  let i;
+  let pt;
   data.url = split[0] || '';
   if (split[1]) {
     bp = (split[1] || '').split('|');
@@ -190,14 +184,14 @@ function device_pixel_ratio() {
 }
 
 function cssSizeInPixel(size) {
-  var rootFontSize;
-  var baseFontSize;
-  var m;
-  var sign;
-  var num;
-  var unit;
-  var cssSizeRegex = /^(-?)([0-9.]*[0-9])([a-z]+)$/i;
-  var matches = cssSizeRegex.exec(size.toString());
+  let rootFontSize;
+  let baseFontSize;
+  let m;
+  let sign;
+  let num;
+  let unit;
+  const cssSizeRegex = /^(-?)([0-9.]*[0-9])([a-z]+)$/i;
+  const matches = cssSizeRegex.exec(size.toString());
   if (matches) {
     // get root font size in pixel
     rootFontSize = window.getComputedStyle($('html')[0])['font-size'];
@@ -229,11 +223,10 @@ function parse_tags(str) {
   return str.replace(hash_regex, '<a href="#tags/$1">#$1</a>');
 }
 
-function remove_duplicate_tags( catagories, content ) {
-  const tagsInContent = saw(content).match(/#\w+/g).map(function(h){
-    return h.replace(/#/, '');
-  }).toArray();
-  return _.difference( catagories, tagsInContent );
+function remove_duplicate_tags(catagories, content) {
+  const tagsInContent = saw(content).match(/#\w+/g).map(h => h.replace(/#/, ''))
+  .toArray();
+  return _.difference(catagories, tagsInContent);
 }
 
 extend(utility, {
